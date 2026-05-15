8008f6b8c5d062e6d8b2c6134920846004a636ad
Ankit kumar Singh <ankit12030621028@gmail.com>
2026-05-15 15:40:06 +0530

    Fixed API base URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,9 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-
-  baseURL: "https://pulseboard-o4dg.onrender.com"
-
+  baseURL: "https://pulseboard-o4dg.onrender.com/api"
 })
 
 export default api
